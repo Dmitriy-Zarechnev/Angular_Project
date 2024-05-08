1cbcefd0692c91eb7a821c8d8ddf95b74ab0ef88
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-05-08 07:39:03 +0300

    08.05 - test2 - 1

---
+++ src/app/test1/test1.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'app-test1',
+  templateUrl: './test1.component.html',
+  standalone: true,
+  styleUrls: ['./test1.component.css']
+})
+
+
+export class Test1Component {
+
+}
