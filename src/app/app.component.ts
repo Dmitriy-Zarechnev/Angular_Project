f35bd71f74f1d212f1eec6535d0ab1f68f235d86
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-05-08 07:43:39 +0300

    08.05 - test5 - 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Test1Component} from './test1/test1.component'
+import {Test2Component} from './test2/test2.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Test1Component],
+  imports: [RouterOutlet, Test1Component, Test2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
