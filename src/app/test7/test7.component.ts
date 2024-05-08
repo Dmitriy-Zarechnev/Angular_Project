b43a668581c7f22d2e8d43b4a151dadcaab5901e
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-05-08 08:00:40 +0300

    08.05 - test5 - 3

---
+++ src/app/test7/test7.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'proj-test7',
+  standalone: true,
+  imports: [],
+  templateUrl: './test7.component.html',
+  styleUrl: './test7.component.css'
+})
+
+export class Test7Component {
+
+}
