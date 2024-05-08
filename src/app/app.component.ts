3d2d3d4c4f844b9531fb6a35838a048cb1ec3002
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-05-08 11:18:15 +0300

    08.05 - binding - 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,54 @@
 import {Component} from '@angular/core'
 import {RouterOutlet} from '@angular/router'
+import {FormsModule} from '@angular/forms'
 
-interface IUser {
-  age: number,
-  name: string
-}
+// interface IUser {
+//   age: number,
+//   name: string
+// }
 
 @Component({
   selector: 'proj-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  appTitle = 'Angular_Project'
 
-  user: IUser = {
-    age: 25,
-    name: 'Bob'
-  }
+export class AppComponent {
+  appTitle = 'Angular'
+  text = 'start'
 }
+
+// export class AppComponent {
+//   appTitle = 'Angular'
+//   text = 'start'
+//
+//   changeTextHandler(event: Event) {
+//     this.text = (event.currentTarget as HTMLInputElement).value
+//   }
+//
+//   changeTitleHandler() {
+//     this.appTitle = 'Hello'
+//   }
+// }
+
+// export class AppComponent {
+//   appTitle = 'Angular_Project'
+//
+//   user: IUser = {
+//     age: 25,
+//     name: 'Bob'
+//   }
+// }
+
+// export class AppComponent {
+//   isAppLoading = true
+//
+//   constructor() {
+//     setTimeout(() => {
+//       this.isAppLoading = false
+//     }, 3000)
+//   }
+//
+// }
