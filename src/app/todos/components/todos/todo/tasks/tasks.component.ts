3c205494353a5386523959c20d8192061e5609df
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-07-02 15:56:09 +0300

    02.07 - lesson 16.4 - completed

--- src/app/todos/components/todos/todo/tasks/tasks.component.ts
+++ src/app/todos/components/todos/todo/tasks/tasks.component.ts
@@ -1,7 +1,9 @@
 import {Component, Input} from '@angular/core'
 import {TasksService} from '../../../../services/tasks.service'
-import {map, Observable} from 'rxjs'
+import {combineLatest, map, Observable} from 'rxjs'
 import {DeleteTask, Task, UpdateTaskModel} from '../../../../models/task.models'
+import {TodosService} from '../../../../services/todos.service'
+import {TaskStatus} from '../../../../../core/enums/taskStatus.enum'
 
 @Component({
   selector: 'tl-tasks',
@@ -17,15 +19,35 @@ export class TasksComponent {
   // ---- Task title при создании ----
   taskTitle = ''
 
+
   // ---- Подключили service для работы с tasks ----
-  constructor(private tasksService: TasksService) {
+  constructor(private tasksService: TasksService, private todosService: TodosService) {
   }
 
   ngOnInit(): void {
-    // subscribe
-    this.tasks$ = this.tasksService.tasks$
-      .pipe(map(tasks => {
-        return tasks[this.todoId]
+
+    // Объединили потоки tasks и todos и subscribe
+    this.tasks$ = combineLatest([this.tasksService.tasks$, this.todosService.todos$])
+      .pipe(map(res => {
+        // Получили все tasks
+        const tasks = res[0]
+        // Нашли нужные по todoId
+        let tasksForTodo = tasks[this.todoId]
+
+        // Получили все todos
+        const todos = res[1]
+        // Нашли нужный по todoId
+        const activeTodo = todos.find(todo => todo.id === this.todoId)
+
+        // Отфильтровали tasks
+        switch (activeTodo?.filter) {
+          case 'completed':
+            return tasksForTodo = tasksForTodo.filter(task => task.status === TaskStatus.completed)
+          case 'active':
+            return tasksForTodo = tasksForTodo.filter(task => task.status === TaskStatus.active)
+          default:
+            return tasksForTodo
+        }
       }))
 
     // Получили tasks с сервера
