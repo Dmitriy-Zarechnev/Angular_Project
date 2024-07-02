4e9a767e6683a0b88954cb419d5ce1aa2b1e0019
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-07-02 14:28:14 +0300

    02.07 - lesson 16.3

---
+++ src/app/todos/components/todos/todo/todo-filter/todo-filter.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'tl-todo-filter',
+  templateUrl: './todo-filter.component.html',
+  styleUrls: ['./todo-filter.component.css']
+})
+
+export class TodoFilterComponent {
+
+}
--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -6,10 +6,12 @@ import {TodoComponent} from './components/todos/todo/todo.component'
 import {FormsModule} from '@angular/forms'
 import {TasksComponent} from './components/todos/todo/tasks/tasks.component'
 import {TaskComponent} from './components/todos/todo/tasks/task/task.component'
+import {TodoFilterComponent} from './components/todos/todo/todo-filter/todo-filter.component'
+import {TodoFooterComponent} from './components/todos/todo/todo-footer/todo-footer.component'
 
 
 @NgModule({
-  declarations: [TodosComponent, TodoComponent, TasksComponent, TaskComponent],
+  declarations: [TodosComponent, TodoComponent, TasksComponent, TaskComponent, TodoFilterComponent, TodoFooterComponent],
   imports: [
     CommonModule, FormsModule, ToDosRoutingModule
   ]
